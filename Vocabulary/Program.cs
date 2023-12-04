423630714ffc6022f97f6407e40b3d45117870ac
SARTE-PC\insom <ceo@sarte.kr>
2023-12-04 23:12:47 +0900

    1204 variable

--- Vocabulary/Program.cs
+++ Vocabulary/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿double heightInMetres = 1.88;
+Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+
+char leteer = 'A';
+char digit = '1';
+char symbol = '$';
+
+string name = "Bob";
+
+string firstName = "Bob";
+string lastName = "Smith";
+string phoneNumber = "(215) 555-4256";
